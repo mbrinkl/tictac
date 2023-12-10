6b4d27ab8540b13f2250d7654ce19848df3aa4f4
Michael Brinkley <bjbrinkleyyy@yahoo.com>
2023-12-10 15:06:44 -0600

    join and spectate tabs

--- packages/client/src/routes/+page.svelte
+++ packages/client/src/routes/+page.svelte
@@ -2,8 +2,10 @@
 	import { goto } from '$app/navigation';
 	import { joinLobby, lobbyRooms } from '$lib/lobby';
 	import { getClient } from '$lib/util';
+	import { Tab, TabGroup } from '@skeletonlabs/skeleton';
 
 	const client = getClient();
+	let tabSet: number = 0;
 
 	const createPublic = () => {
 		goto('/rooms/create');
@@ -25,28 +27,41 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<div class="table-container mb-3">
-	<table class="table table-hover table-compact">
-		<thead>
-			<tr>
-				<th>ID</th>
-				<th>Created By</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each $lobbyRooms as room}
-				<tr class="cursor-pointer" on:click={() => join(room.roomId)}>
-					<td>{room.roomId}</td>
-					<td>...</td>
-				</tr>
-			{:else}
-				<tr>
-					<td colspan={2}>No Games to Join</td>
-				</tr>
-			{/each}
-		</tbody>
-	</table>
-</div>
+<TabGroup justify="justify-center">
+	<Tab bind:group={tabSet} name="joinTab" value={0}>Join</Tab>
+	<Tab bind:group={tabSet} name="spectateTab" value={1}>Spectate</Tab>
+
+	<svelte:fragment slot="panel">
+		<div class="table-container mb-3">
+			<table class="table table-hover table-compact">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Created By</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#if tabSet === 0}
+						{#each $lobbyRooms as room}
+							<tr class="cursor-pointer" on:click={() => join(room.roomId)}>
+								<td>{room.roomId}</td>
+								<td>...</td>
+							</tr>
+						{:else}
+							<tr>
+								<td colspan={2}>No Games to Join</td>
+							</tr>
+						{/each}
+					{:else}
+						<tr>
+							<td colspan={2}>No Games to Spectate</td>
+						</tr>
+					{/if}
+				</tbody>
+			</table>
+		</div>
+	</svelte:fragment>
+</TabGroup>
 
 <div class="flex justify-center gap-3">
 	<button class="btn variant-ghost-primary" on:click={createPublic}>Create Public</button>
