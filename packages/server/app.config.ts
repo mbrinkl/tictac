0bb02e6c494d495b8d878be428205404e20a764c
Michael Brinkley <bjbrinkleyyy@yahoo.com>
2023-12-09 23:35:43 -0600

    staticfiles served from express

--- packages/server/app.config.ts
+++ packages/server/app.config.ts
@@ -3,23 +3,26 @@ import { monitor } from '@colyseus/monitor';
 import { playground } from '@colyseus/playground';
 import { BattleRoom } from './rooms/BattleRoom';
 import { LobbyRoom } from 'colyseus';
+import express from 'express';
 
 export default config({
-  initializeGameServer: (gameServer) => {
-    gameServer.define('lobby', LobbyRoom);
-    gameServer.define('battle', BattleRoom).enableRealtimeListing();
-  },
+	initializeGameServer: (gameServer) => {
+		gameServer.define('lobby', LobbyRoom);
+		gameServer.define('battle', BattleRoom).enableRealtimeListing();
+	},
 
-  initializeExpress: (app) => {
-    if (process.env.NODE_ENV !== 'production') {
-      app.use('/', playground);
-    }
+	initializeExpress: (app) => {
+		if (process.env.NODE_ENV !== 'production') {
+			app.use('/', playground);
+		} else {
+			app.use(express.static('../client/build'));
+		}
 
-    /**
-     * Use @colyseus/monitor
-     * It is recommended to protect this route with a password
-     * Read more: https://docs.colyseus.io/tools/monitor/#restrict-access-to-the-panel-using-a-password
-     */
-    app.use('/colyseus', monitor());
-  },
+		/**
+		 * Use @colyseus/monitor
+		 * It is recommended to protect this route with a password
+		 * Read more: https://docs.colyseus.io/tools/monitor/#restrict-access-to-the-panel-using-a-password
+		 */
+		app.use('/colyseus', monitor());
+	},
 });
